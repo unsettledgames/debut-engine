f50ef6ef33415a05e7b0c657574831c34ad17a14
Nicola <nicpal2000@gmail.com>
2023-01-04 23:13:55 +0100

    Integrated mono in CMakeLists

--- scripts/Win-GenMSVC19.bat
+++
@@ -1,4 +0,0 @@
-pushd .\..\
-call cmake -G "Visual Studio 16 2019"
-popd
-PAUSE
--- scripts/Win-GenMSVC22.bat
+++ scripts/Win-GenMSVC22.bat
@@ -1,6 +1,10 @@
 pushd .\..
 mkdir Build
 cd Build
-call cmake -G "Visual Studio 17 2022" -DWINDOWS_BUILD=ON ./..
+call cmake -G "Visual Studio 17 2022" -DWINDOWS_BUILD=ON .\..
+copy .\..\Debut\vendor\mono\bin\mono-2.0-sgen.dll Debutant\Build\Release
+copy .\..\Debut\vendor\mono\bin\mono-2.0-sgen.dll Debutant\Build\Debug
+copy .\..\Debut\vendor\mono\bin\MonoPosixHelper.dll Debutant\Build\Release
+copy .\..\Debut\vendor\mono\bin\MonoPosixHelper.dll Debutant\Build\Debug
 popd
 PAUSE
